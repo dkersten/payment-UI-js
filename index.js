facd0845b4520774454d96c20dc542593a5a35df
dkersten <dkersten1994@gmail.com>
2020-08-26 23:45:48 -0400

    initial functionality complete

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
-const inputName = document.querySelector('.name')
-const inputNum = document.querySelector('.card-num')
-const inputDate = document.querySelector('.date')
+// set inputs to vars
+const inputName = document.querySelector('.input-name')
+const inputNum = document.querySelector('input.input-num')
+const inputDate = document.querySelector('input.input-date')
 
-inputName.textContent ='John Doe'
-inputNum.textContent = '1234 5678 9012 3456'
-inputDate.textContent = '10/22'
+// set card elements to vars
+const cardName = document.querySelector('.name')
+const cardNum = document.querySelector('.card-num')
+const cardDate = document.querySelector('.date')
+
+// set placeholder content on load
+cardName.textContent ='John Doe'
+cardNum.textContent = '1234 5678 9012 3456'
+cardDate.textContent = '10/22'
+
+// event handler for CC owner name
+inputName.addEventListener('keyup', (e) => {
+	cardName.textContent = e.target.value
+})
+
+// event handler for CC number
+inputNum.addEventListener('keyup', (e) => {
+	let ccNum = e.target.value
+	let formatedNum = ccNum.match(/.{1,4}/g)
+	cardNum.textContent = formatedNum.join(' ')
+})
+
+// event handler for CC date
+inputDate.addEventListener('change', (e) => {
+	cardDate.textContent = e.target.value
+})
+
+// event handler for form submit
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	cardName.textContent ='John Doe'
+	cardNum.textContent = '1234 5678 9012 3456'
+	cardDate.textContent = '10/22'
+	inputName.target.value = ''
+	inputNum.target.value = ''
+	inputDate.target.value = ''
+})
