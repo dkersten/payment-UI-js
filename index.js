77896a38c60856c6218d74e813573610fed25b2e
dkersten <dkersten1994@gmail.com>
2020-08-26 22:49:07 -0400

    updated CC component

---
+++ index.js
@@ -0,0 +1,7 @@
+const inputName = document.querySelector('.name')
+const inputNum = document.querySelector('.card-num')
+const inputDate = document.querySelector('.date')
+
+inputName.textContent ='John Doe'
+inputNum.textContent = '1234 5678 9012 3456'
+inputDate.textContent = '10/22'
